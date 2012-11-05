0df9d8475459f1d5c6ffc4c9622f0e1bee655f1f
Carl Whittaker <carl.whittaker@unboxedconsulting.com>
2012-11-05 15:55:19 +0000

    Correcting spec description

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 var subject = require('../');
 
 describe('RGBToHSL(r, g, b)', function () {
-  it('should return the an array of values', function () {
+  it('should return an array of values', function () {
     subject(255, 150, 0).should.eql([35.294117647058826, -1.007905138339921, 127.5]);
   });
 });
